dff09f2887fda12b33ccd6c73729ccdcf2e5b00a
Pietrodjaowjao <nicoleobenetti@gmail.com>
2022-01-30 04:14:04 -0300

    i will sleep again

--- source/Controls.hx
+++ source/Controls.hx
@@ -10,6 +10,7 @@ import flixel.input.actions.FlxActionSet;
 import flixel.input.gamepad.FlxGamepadButton;
 import flixel.input.gamepad.FlxGamepadInputID;
 import flixel.input.keyboard.FlxKey;
+import openfl.events.KeyboardEvent;
 #if android
 import flixel.group.FlxGroup;
 import ui.Hitbox;
